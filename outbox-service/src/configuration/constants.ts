142d96da871fb6775d3229de54a9960ea1805ff4
$AMLRT-app <Mohamed.ragab@integrant.com>
2020-08-02 05:02:42 +0200

    fix docker-compose file

--- outbox-service/src/configuration/constants.ts
+++ outbox-service/src/configuration/constants.ts
@@ -1,3 +1,3 @@
-export const KAFKA_BROKER = 'localhost:9092'
+export const KAFKA_BROKER = 'localhost:9093'
 export const KAFKA_CLIENT_ID = 'outbox-service'
 export const KAFAK_ORDER_SEND_TOPIC = 'order-send'
