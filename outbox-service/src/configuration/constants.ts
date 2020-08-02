bd32a285830f5920233d22805cd07cbd0630604f
$AMLRT-app <Mohamed.ragab@integrant.com>
2020-08-02 13:15:39 +0200

    change kafka image

--- outbox-service/src/configuration/constants.ts
+++ outbox-service/src/configuration/constants.ts
@@ -1,3 +1,3 @@
-export const KAFKA_BROKER = 'kafka-server1:9093'
+export const KAFKA_BROKER = 'kafka-server1:9092'
 export const KAFKA_CLIENT_ID = 'outbox-service'
 export const KAFAK_ORDER_SEND_TOPIC = 'order-send'
