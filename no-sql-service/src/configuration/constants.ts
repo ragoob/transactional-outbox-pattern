443c977649b8f8a0c0672430c9c57bb11c87a889
$AMLRT-app <Mohamed.ragab@integrant.com>
2020-08-02 12:39:58 +0200

    change kafka broker server

--- no-sql-service/src/configuration/constants.ts
+++ no-sql-service/src/configuration/constants.ts
@@ -1,3 +1,3 @@
-export const KAFKA_BROKER = 'localhost:9093'
+export const KAFKA_BROKER = 'kafka-server1:9093'
 export const KAFAKA_ORDERR_READ_TOPIC = 'order-send'
 export const KAFKA_GROUP_ID = 'log-service'
--- outbox-service/src/configuration/constants.ts
+++ outbox-service/src/configuration/constants.ts
@@ -1,3 +1,3 @@
-export const KAFKA_BROKER = 'localhost:9093'
+export const KAFKA_BROKER = 'kafka-server1:9093'
 export const KAFKA_CLIENT_ID = 'outbox-service'
 export const KAFAK_ORDER_SEND_TOPIC = 'order-send'
